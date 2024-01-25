fa9f871c532e747a1b9358c53abf20a7229fd240
AithaSupriya <157452696+AithaSupriya@users.noreply.github.com>
2024-01-25 21:03:39 +0530

    Add files via upload

---
+++ ClassAddition.java
@@ -0,0 +1,14 @@
+package com.anp6589;
+
+public class ClassAddition {
+	ClassAddition obj1= new ClassAddition();
+	public static void main(String[] args) {
+		System.out.print("Addition of x and y is ");
+		PrintAddition();
+	}
+static void PrintAddition()
+{
+	int x=35,y=15;
+	System.out.println(x+y);
+}
+}
