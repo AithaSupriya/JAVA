d2bc01a40097148d293fa89fd8ac812d8fe9ef8c
AithaSupriya <157452696+AithaSupriya@users.noreply.github.com>
2024-01-25 21:03:00 +0530

    Add files via upload

---
+++ ClassPrintName.java
@@ -0,0 +1,13 @@
+package com.anp6589;
+
+public class ClassPrintName {
+	public static void main(String[] args) {
+		ClassPrintName obj1 = new ClassPrintName();
+		System.out.print("Hello ");
+		obj1.PrintName();
+	}
+void PrintName() {
+	System.out.print("Supriya");
+}
+
+}
