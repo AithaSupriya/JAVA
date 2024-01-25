a8c09e0b8e68bbd5eb1411a079dcdace24582731
AithaSupriya <157452696+AithaSupriya@users.noreply.github.com>
2024-01-25 21:01:06 +0530

    Add files via upload

---
+++ MyInfo.java
@@ -0,0 +1,17 @@
+package com.anp6589;
+
+public class MyInfo {
+	public static void main(String[] args) {
+		String name= "AITHA SUPRIYA";
+		int age=21;
+		String Clg= "St.Mary's Engineering College";
+		String Stream= "Computer Science and Engineering";
+		int YOP= 2023;
+		 String City= "Hyderabad";
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Clg: "+Clg);
+		System.out.println("Stream:"+Stream);
+		System.out.println("YOP: "+YOP);
+		System.out.println("City: "+City);	}
+}
