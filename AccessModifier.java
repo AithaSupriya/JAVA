0851ffc5fdb0835fc4d881d1c3ae24c6b32bf780
AithaSupriya <157452696+AithaSupriya@users.noreply.github.com>
2024-01-25 21:08:41 +0530

    Update AccessModifier.java

--- AccessModifier.java
+++ AccessModifier.java
@@ -34,22 +34,6 @@ o/p:This is default method
 	30
 
  */
-========================================================================
-package com.anp6589;
-
-public class AccessModifiers2 {
-	public static void main(String[] args) {
-		AccessModifier AM=new AccessModifier();
-		AM.publicexample();//this method can be called 
-		//AM.privatexample(); //this cannot be called bcoz private modifier won't give access to the other classes 
-		AM.defaultexample();//this method can be called
-	}
-}
-
-/*
-o/p:This is public method:
-	30
-	This is default method
 
 
 */
